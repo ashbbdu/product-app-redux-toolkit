46e8c1b07b1d4922c2bf0f894f98f5adbf3afba6
Ashish Srivastava <68013078+ashbbdu@users.noreply.github.com>
2024-02-01 11:32:38 +0530

    Update cartSlice.js

--- src/redux/Slices/cartSlice.js
+++ src/redux/Slices/cartSlice.js
@@ -9,7 +9,6 @@ export const cartSlice = createSlice({
             state.push(action.payload)
         },
         removeFromCard : (state , action ) => {
-            // meaans state k andr , jo id select hoke aai hai usko chod k baki sari id retain krna
            return  state.filter(item => item.id !== action.payload )
         }
     }
